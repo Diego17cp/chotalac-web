b448a18ec718f583123e0495814c00eb8658176e
Diego17 <diegoalbertocastropastor@gmail.com>
2025-12-06 22:27:21 -0500

    Add CAROUSEL_MOCK constant and update index export

---
+++ web/src/constants/carousel.tsx
@@ -0,0 +1,14 @@
+import type { CarouselItem } from "@/types";
+
+export const CAROUSEL_MOCK: CarouselItem[] = [
+    {
+        id: 1,
+        title: "Queso de Corte",
+        imageUrl: "/images/carousel/queso-de-corte.webp",
+    },
+    {
+        id: 1,
+        title: "Queso de Corte",
+        imageUrl: "/images/carousel/queso-de-corte.webp",
+    },
+]
