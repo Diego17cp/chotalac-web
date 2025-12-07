c1000776d195c3f24c67a63d4bcc38f6e3a7851d
Diego17 <diegoalbertocastropastor@gmail.com>
2025-12-06 19:44:05 -0500

    Add navItems constant for navigation links

---
+++ web/src/constants/navItems.ts
@@ -0,0 +1,8 @@
+import type { NavItem } from "@/types";
+
+export const NAV_ITEMS: NavItem[] = [
+	{ label: "Inicio", href: "/" },
+	{ label: "Productos", href: "/productos" },
+	{ label: "Sobre Nosotros", href: "/sobre-nosotros" },
+	{ label: "Contáctanos", href: "/contacto" },
+];
