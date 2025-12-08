748e73227fb74a0eaa20aef9e8a66fd9a1448dd2
Diego17 <diegoalbertocastropastor@gmail.com>
2025-12-08 17:18:51 -0500

    Add About Us section and update index to include it in the layout

--- web/src/components/home/index.ts
+++ web/src/components/home/index.ts
@@ -1,2 +1,3 @@
 export { default as Carousel } from "./Carousel.astro";
 export { default as FeaturedProductsSection } from "./FeaturedProductsSection.astro";
+export { default as AboutUs } from "./AboutUs.astro";
