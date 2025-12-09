b5eb79211d099a6e3bb4ff7cd887def6be1961fb
Diego17 <diegoalbertocastropastor@gmail.com>
2025-12-08 21:18:31 -0500

    Add InvitationSection component to showcase the transformation of fresh milk

--- web/src/components/about-us/index.ts
+++ web/src/components/about-us/index.ts
@@ -1,1 +1,6 @@
 export { default as AboutUsSection } from './AboutUsSection.astro';
+export { default as OurMissionVisionSection } from './MVVSection.astro';
+export { default as OurHistorySection } from './OurHistorySection.astro';
+export { default as OurFacilitiesSection } from './OurFacilitiesSection.astro';
+export { default as OurProcessesSection } from './OurProcessesSection.astro';
+export { default as InvitationSection } from './InvitationSection.astro';
