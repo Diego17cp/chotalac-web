54d172653ca5a253cd7e05dc95aff0bb1de7e583
Diego17 <diegoalbertocastropastor@gmail.com>
2025-12-09 15:21:30 -0500

    Add ProductsGrid component for displaying product categories

--- web/src/components/categories/index.ts
+++ web/src/components/categories/index.ts
@@ -2,3 +2,6 @@ export { default as Banner } from "./Banner.astro";
 export { default as CategoriesSection } from "./CategoriesSection.astro";
 export { default as CategoryItem } from "./CategoryItem.astro";
 export { default as NotFound } from "./NotFound.astro";
+export { default as ProductsGrid } from './ProductsGrid.astro';
+export { default as ProductSection } from './ProductSection.astro';
+export { default as PresentationCard } from './PresentationCard.astro';
