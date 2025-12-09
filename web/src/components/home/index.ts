3da1e3bae4659ba5a1b3c5f08cbf93ca3d3eb1e5
Diego17 <diegoalbertocastropastor@gmail.com>
2025-12-08 19:41:45 -0500

    Add OurFacilities component and update index to include it

--- web/src/components/home/index.ts
+++ web/src/components/home/index.ts
@@ -1,3 +1,4 @@
 export { default as Carousel } from "./Carousel.astro";
 export { default as FeaturedProductsSection } from "./FeaturedProductsSection.astro";
 export { default as AboutUs } from "./AboutUs.astro";
+export { default as OurFacilities } from "./OurFacilities.astro";
