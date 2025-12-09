15e0b228129f950519e01892a1ec3dbea5052f27
Diego17 <diegoalbertocastropastor@gmail.com>
2025-12-09 14:58:51 -0500

    Add NotFound component for handling missing categories

--- web/src/components/categories/index.ts
+++ web/src/components/categories/index.ts
@@ -1,2 +1,4 @@
 export { default as Banner } from "./Banner.astro";
 export { default as CategoriesSection } from "./CategoriesSection.astro";
+export { default as CategoryItem } from "./CategoryItem.astro";
+export { default as NotFound } from "./NotFound.astro";
