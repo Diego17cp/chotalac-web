528ed3bf7b88cf15e9cb0c6036a9ed8fbbc07100
Diego17 <diegoalbertocastropastor@gmail.com>
2025-12-09 22:42:00 -0500

    Add RelatedProducts component for displaying related products with dynamic background colors

---
+++ web/src/components/products/index.ts
@@ -0,0 +1,5 @@
+export { default as ProductGallery } from "./ProductGallery.astro";
+export { default as ProductInfo } from "./ProductInfo.astro";
+export { default as ProductDetails } from "./ProductDetails.astro";
+export { default as RelatedProducts } from "./RelatedProducts.astro";
+export { default as NotFound } from "./NotFound.astro";
