52709c801ffa48ba089bdfc3e5b8c78c5a67e7ea
Diego17 <diegoalbertocastropastor@gmail.com>
2025-12-21 10:04:17 -0500

    Add social media links to footer and create SOCIAL_LINKS constant

---
+++ web/src/constants/socialLinks.ts
@@ -0,0 +1,6 @@
+export const SOCIAL_LINKS = {
+    FACEBOOK: "https://www.facebook.com/share/17p3ijD8Jp/",
+    INSTAGRAM: "https://www.instagram.com/chotalac.oficial?igsh=MTVpZjcyb2p1MjN2cA==",
+    TIKTOK: "https://www.tiktok.com/@chotalac.oficial?_r=1&_t=ZS-92AfnvSAjx7",
+    WHATSAPP: "https://wa.me/51910017026",
+}
