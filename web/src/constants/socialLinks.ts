cc58a2f55fb4a6ae544271dc04bca279367da635
Diego17 <diegoalbertocastropastor@gmail.com>
2026-01-26 11:10:00 -0500

    Update footer address and enhance WhatsApp links for product inquiries

--- web/src/constants/socialLinks.ts
+++ web/src/constants/socialLinks.ts
@@ -2,5 +2,6 @@ export const SOCIAL_LINKS = {
     FACEBOOK: "https://www.facebook.com/share/17p3ijD8Jp/",
     INSTAGRAM: "https://www.instagram.com/chotalac.oficial?igsh=MTVpZjcyb2p1MjN2cA==",
     TIKTOK: "https://www.tiktok.com/@chotalac.oficial?_r=1&_t=ZS-92AfnvSAjx7",
-    WHATSAPP: "https://wa.me/51910017026",
+    WHATSAPP: "https://wa.me/51934149055?text=Hola, estoy interesado en sus productos",
+    WHATSAPP_PRODUCTS: "https://wa.me/51934149055?text=Hola, estoy interesado en el producto: ",
 }
